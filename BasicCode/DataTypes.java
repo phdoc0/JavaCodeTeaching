09e329224754a9bb765b5e7c562b9d64694db21d
MatthewR2D2 <matthew.c.r.millar@gmail.com>
2021-01-24 13:45:22 -0500

    Added two basic starter code to Git

---
+++ BasicCode/DataTypes.java
@@ -0,0 +1,56 @@
+package BasicCode;
+
+/**
+ * This code will intorduce Data types and how to use them
+ */
+public class DataTypes {
+    
+    public static void main(String[] args){
+        System.out.println("Starting Program");
+
+        //Integer 
+        /**
+         * Integer types can hold whole numbers such as 123 and −96. 
+         * The size of the values that can be stored depends on the integer type that we choose.
+         */
+
+        int myInt = 10;
+
+         /**
+          * Floatint point (Float)  
+          * Floating point data types are used to represent numbers with a fractional part. 
+          * Single precision floating point numbers occupy 4 bytes and Double precision floating point numbers occupy 8 bytes.
+          */
+
+        float myFloat = 10;
+
+        /**
+         * Double
+         * Double can hold decimal places and very large numbers compared to Flote and integer
+         */
+
+        double myDouble = 10.10;
+
+        /**
+         * Char
+         * t stores character constants in the memory. 
+         * It assumes a size of 2 bytes, but basically it can hold only a single character because char stores unicode character sets. 
+         */
+        
+        char myChar = 'a';
+
+        /**
+         * Boolean data types are used to store values with two states: true or false.
+         */
+
+        boolean myBool = true;
+
+        /**
+         * String
+         * Hold a array of chars to form words and sentences. 
+         */
+
+        String myString = "A word or sentence.";
+
+    }
+}
---
+++ BasicCode/HelloWorld.java
@@ -0,0 +1,15 @@
+package BasicCode;
+/**
+ * This is the most basic program for printing a statement in Java
+ * Simple and easy to follow
+ * Most simpletest java code you can have.
+ */
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("New Line for hello world");
+
+        System.out.print("First Sentence: ");
+        System.out.print("Second Sentnence on same line");
+    }
+}
