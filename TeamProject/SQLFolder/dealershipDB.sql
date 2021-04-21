719cfb1e556ba0883ce500289f6a54e4b189cb9d
becktrb <78628885+becktrb@users.noreply.github.com>
2021-04-20 20:44:49 -0400

    Update dealershipDB.sql

--- TeamProject/SQLFolder/dealershipDB.sql
+++ TeamProject/SQLFolder/dealershipDB.sql
@@ -5,5 +5,5 @@ drop table if exists invoices;
 drop table if exists customers;
 create table invoices ();
 create table cars ();
-create table customers (customer_id int(10) not NULL AUTO_INCREMENT , name VARCHAR (50) NOT null, address VARCHAR (60) NOT null, phone VARCHAR(10) NOT NULL , email VARCHAR(20) ,PRIMARY KEY (customer_id), FOREIGN KEY (invoice_id) REFERENCES invoices(invoice_id));
+create table customers (customer_id int(10) not NULL AUTO_INCREMENT , name VARCHAR (50) NOT null, address VARCHAR (60) NOT null, phone VARCHAR(10) NOT NULL , email VARCHAR(20) ,invoice_id int (10) not null, PRIMARY KEY (customer_id), FOREIGN KEY (invoice_id) REFERENCES invoices(invoice_id));
 
