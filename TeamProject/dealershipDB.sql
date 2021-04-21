d1f2f68012801569380ea044840cf7e06dc59b4b
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-04-20 20:23:51 -0400

    Add files via upload
    starter file for the db

---
+++ TeamProject/dealershipDB.sql
@@ -0,0 +1,8 @@
+create DATABASE if not exists dealership;
+use dealership;
+drop table if exists cars;
+drop table if exists invoices;
+drop table if exists customers;
+create table cars ();
+create table customers ();
+create table invoices ();
