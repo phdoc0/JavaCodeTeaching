3a129e74a65984b37251cfe8bfcbb5f544d393cf
becktrb <78628885+becktrb@users.noreply.github.com>
2021-04-20 20:51:11 -0400

    Update dealershipDB.sql

--- TeamProject/SQLFolder/dealershipDB.sql
+++ TeamProject/SQLFolder/dealershipDB.sql
@@ -7,7 +7,7 @@ create table invoices ();
 create table cars ();
 create table customers (customer_id int(10) not NULL AUTO_INCREMENT , name VARCHAR (50) NOT null, 
                         address VARCHAR (60) NOT null, phone VARCHAR(10) NOT NULL , email VARCHAR(20) ,
-                        invoice_id int (10) not null, PRIMARY KEY (customer_id), 
+                        invoice_id int (10) not null, car_id int(10) not null, PRIMARY KEY (customer_id), 
                         FOREIGN KEY (car_id) REFERENCES cars(car_id),
                         FOREIGN KEY (invoice_id) REFERENCES invoices(invoice_id));
 
